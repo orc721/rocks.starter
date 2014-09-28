29fe4c0deee5d7e5bfa44e02d34d04015f21dcb5
Gerald Bauer <gerald.bauer@gmail.com>
2014-09-28 20:38:24 +0200

    Update test.js

--- test.js
+++ test.js
@@ -6,16 +6,16 @@ var sportdb = require( './sportdb' );
 //               name: 'English Premier League 2014/15' } ];
 
 
-sportdb.Event.findAll( function( events ) {
+sportdb.Event.findAll( function( err, events ) {
   console.log( "events:" );
   console.log( events );
 
-  sportdb.Team.findByEvent( events[0], function( teams ) {
+  sportdb.Team.findByEvent( events[0], function( err, teams ) {
     console.log( "teams:" );
     console.log( teams );
   });
 
-  sportdb.Event.findByKey( events[0].key, function( event ) {
+  sportdb.Event.findByKey( events[0].key, function( err, event ) {
     console.log( "event:" );
     console.log( event );
   });
